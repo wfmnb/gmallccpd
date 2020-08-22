95d21ceefd695a02ee2a01916bfd748a9c00edb2
wufaming <real.wu@foxmail.com>
2020-08-22 13:35:01 +0800

    先加入用户管理模块

---
+++ gmall0822-user-manage/src/main/java/com/ccpd/gmall0822/user/Gmall0822UserManageApplication.java
@@ -0,0 +1,13 @@
+package com.ccpd.gmall0822.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0822UserManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gmall0822UserManageApplication.class, args);
+	}
+
+}
